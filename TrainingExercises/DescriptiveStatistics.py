5cc8849aebd916e2c2c2376422d1a34c8a8bb7c8
Mercy Njoki (Techno Brain Global FZE) <v-menjok@microsoft.com>
2024-04-22 15:45:47 +0300

    DescriptiveStatistics

---
+++ TrainingExercises/DescriptiveStatistics.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+data = [10,20,30,40,50]
+
+#Mean
+mean = np.mean(data)
+print("Mean",mean)
+
+#Median
+median = np.median(data)
+print("Median",median)
+
+#Standard Deviation
+stdDev = np.std(data)
+print("Standard Deviation",stdDev)
